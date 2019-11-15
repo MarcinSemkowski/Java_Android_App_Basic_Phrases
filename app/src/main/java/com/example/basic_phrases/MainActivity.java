db9407610bba43004d1066d15532ebabb678d109
Marcin <marcin96semkowski@gmail.com>
2019-11-15 13:22:57 +0100

    App Logic #1

--- app/src/main/java/com/example/basic_phrases/MainActivity.java
+++ app/src/main/java/com/example/basic_phrases/MainActivity.java
@@ -3,8 +3,16 @@ package com.example.basic_phrases;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void buttonTapped(View view){
+        Log.i("Button Tabbed:", "button Click : " + view.getId());
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
